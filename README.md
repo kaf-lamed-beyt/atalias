dd32cad43605fee2564b8ffba794e6ffe733048d
kaf-lamed-beyt <belac335@gmail.com>
2023-04-18 00:27:17 +0100

    chore: modify README, LICENSE. refactor conditions

---
+++ README.md
@@ -0,0 +1,75 @@
+# atalias | [ah tah lias] | (@)-alias
+
+at (@) custom aliases for your Next.js projects, without the forward slashes
+
+## Why?
+
+When you create an app or website with Next.js, you get the option to add custom [import aliases](https://nextjs.org/docs/advanced-features/module-path-aliases). Before this feature, I've used custom aliases and I've been quite attached to using the **at(@)-aliases**
+
+Next.js allows you customize this, actually. So you can decide to use any character you like, to represent your custom aliases. The **at(@)alias** that Next.js provides has a forward-slash in front of it &mdash; i have no idea why it is like that,
+
+And I think it is like that, irrespective of the symbol you decide to use. So you get that trailing forward-slash accompanying your import definitions.
+
+So, a typical component that would appear like this, for me, previously:
+
+```tsx
+import SideBar from "@components/sidebar";
+```
+
+Became:
+
+```tsx
+import SideBar from "@/components/sidebar";
+```
+
+I wasn't satisfied. No. I did not like it. I think my issues are just too much. Most of the time, I'd find myself, editing the `compilerOptions` in `tsconfig.json` &mdash; `jsconfig.json`, majority of the time.
+
+**Why not automate it?** &mdash; And here I am. Hopefully, you'd find it useful.
+
+## Usage
+
+Install the package with npm
+
+```bash
+npm install atalias
+```
+
+**yarn**
+
+```bash
+yarn add atalias
+```
+
+**pnpm**
+
+```bash
+pnpm add atalias
+```
+
+The tool assumes that you have a `src` folder in your project. To create an alias, you can type the command below into your terminal.
+
+```bash
+create-atalias src/components
+```
+
+To create multiple aliases at a time, you can do so by making the entries comma-separated like so:
+
+```bash
+create-atalias src/hooks, src/layouts, src/containers, src/utils
+```
+
+## Options
+
+Say you forgot the aliases you have created, and to avoid conflicting aliases, you can list all the aliases you have in the terminal by adding the `-l` or `--list` flag to **create-atalias** like so:
+
+```bash
+create-atalias --list
+```
+
+## Contributing.
+
+See something that doesn't look good? &mdash; even in this README, typos fit plenty. Shoot a PR.
+
+## LICENSE
+
+[MIT](LICENSE) © 2023 kaf-lamed-beyt
--- index.js
+++ index.js
@@ -18,11 +18,27 @@ async function run() {
     output: process.stdout,
   });
 
-  program.parse(process.argv);
+  program
+    .option("-l, --list-atalias", "Lists your available aliases")
+    .parse(process.argv);
+
+  if (program.list) {
+    console.log(normal("Available aliases: "));
+
+    for (const [alias, path] of Object.entries(
+      configFile.compilerOptions.paths
+    )) {
+      console.log(normal(`${alias} --> ${path.join(", ")}`));
+    }
+
+    rl.close();
+    return;
+  }
 
   const configFile = fs.existsSync("tsconfig.json")
     ? "tsconfig.json"
     : "jsconfig.json";
+
   const config = {
     compilerOptions: {
       paths: {},
@@ -51,8 +67,6 @@ async function run() {
     .filter((dir) => fs.lstatSync(dir).isDirectory())
     .map((dir) => dir.replace(`${mainDir}/`, ""));
 
-  console.log(dirs);
-
   try {
     rl.question(
       info(
@@ -62,46 +76,66 @@ async function run() {
         const inputDirs = answer.split(",").map((dir) => dir.trim());
         const allDirs = [...new Set([...dirs, ...inputDirs])];
 
-        if (!allDirs) {
+        // lets us enforce a pattern of reperesenting the dirs to avoid confusion
+        let isValid = true;
+
+        inputDirs.forEach((dir) => {
+          if (!dir.startsWith(mainDir)) {
+            isValid = false;
+
+            console.log(
+              normal(
+                `you should append ${mainDir}/ to the name of the folder \n`
+              )
+            );
+          }
+        });
+
+        if (!isValid) {
           console.log(
-            warning(`Please type the directories you want to use as aliases`)
+            warning(
+              `and please type the directories you want to use as aliases`
+            )
           );
+
           rl.close();
         } else {
-          // lets us enforce a pattern of reperesenting the dirs to avoid confusion
-          let isValid = true;
-
-          inputDirs.forEach((dir) => {
-            if (!dir.startsWith(mainDir)) {
-              isValid = false;
-
-              console.log(
-                normal(
-                  `you should append ${mainDir}/ to the name of the folder \n`
-                )
-              );
-            }
-
-            rl.close();
-          });
-
           if (isValid) {
             allDirs.forEach((dir) => {
               const alias = `@${dir.replace(`${mainDir}/`, "")}`;
               const path = [`${dir}/*`];
-              config.compilerOptions.paths[alias] = path;
 
-              if (config.compilerOptions.paths.hasOwnProperty(alias)) {
-                console.log(error(`${alias} exists as an alias already.`));
+              // Check if the alias already exists in the paths object
+              let aliasExists = false;
+
+              for (let key in config.compilerOptions.paths) {
+                if (key === alias) {
+                  aliasExists = true;
+                  console.log(error(`\n${alias} already exists as an alias.`));
+                  break;
+                }
+              }
+
+              if (!aliasExists) {
+                config.compilerOptions.paths[alias] = path;
+                console.log(normal(`${alias}...`));
+
+                fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
+                console.log(
+                  success(
+                    allDirs.length > 1
+                      ? "\nall aliases were created successfully! 🚀"
+                      : "\nalias created successfully 🚀"
+                  )
+                );
 
                 rl.close();
+              } else {
+                rl.close();
+                return;
               }
             });
-          } else {
-            fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
-            console.log(success("Aliases created successfully! 🚀"));
           }
-          rl.close();
         }
       }
     );
