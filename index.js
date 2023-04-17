7d0ceedb6fdec308019fbf42ad430ece7014dc6c
kaf-lamed-beyt <belac335@gmail.com>
2023-04-17 15:23:56 +0100

    chore: add chalk

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const glob = require("glob");
 const { program } = require("commander");
 const readline = require("readline");
+import chalk from "chalk";
+
+const info = chalk.bold.blueBright;
+const success = chalk.bold.greenBright;
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -15,7 +19,6 @@ const configFile = fs.existsSync("tsconfig.json")
   : "jsconfig.json";
 const config = {
   compilerOptions: {
-    baseUrl: "./",
     paths: {},
   },
 };
@@ -24,6 +27,14 @@ if (fs.existsSync(configFile)) {
   const contents = fs.readFileSync(configFile, "utf8");
   const parsed = JSON.parse(contents);
   config.compilerOptions = parsed.compilerOptions || {};
+}
+
+if (!config.compilerOptions.baseUrl) {
+  config.compilerOptions.baseUrl = "./";
+}
+
+if (!config.compilerOptions.paths) {
+  config.compilerOptions.paths = {};
 }
 
 const mainDir = "src";
@@ -35,20 +46,22 @@ const dirs = glob
   .map((dir) => dir.replace(`${mainDir}/`, ""));
 
 rl.question(
-  `Enter additional directories to add as aliases, separated by commas (e.g. ${mainDir}/utils,${mainDir}/components): `,
+  info(
+    `Enter additional directories to add as aliases, separated by commas (e.g. ${mainDir}/utils,${mainDir}/components):`
+  ),
   (answer) => {
     const inputDirs = answer.split(",").map((dir) => dir.trim());
 
     const allDirs = [...new Set([...dirs, ...inputDirs])];
 
     allDirs.forEach((dir) => {
-      const alias = `@${dir.replace("src/", "")}/*`;
+      const alias = `@${dir.replace(mainDir + "/", "")}/*`;
       const path = [`${dir}/*`];
       config.compilerOptions.paths[alias] = path;
     });
 
     fs.writeFileSync(configFile, JSON.stringify(config, null, 2));
-    console.log("Aliases created successfully!");
+    console.log(success("Aliases created successfully! 🚀 "));
     rl.close();
   }
 );
